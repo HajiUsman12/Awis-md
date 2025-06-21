15fbae9b50f3a8dde609a2731c0941c657d491f1
HajiUsman12 <164213796+HajiUsman12@users.noreply.github.com>
2025-06-21 22:08:30 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "yhBSBCBB#HYpsmq9-mY8_UAzKrE27U4-oTw2W-36_9yPC0389zO0",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    CAPTION: process.env.CAPTION || "*ᴘᴏᴡᴅᴇʀᴇᴅ ʙʏ HASAN*",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
+    AUTO_TYPING: process.env.AUTO_TYPING || "true",
+    CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTHOR: (process.env.PACK_INFO?.split(';') || [])[0] || 'HASAN MD',
+    PACKNAME: (process.env.PACK_INFO?.split(';') || [])[1] || '923190217441',
+    AUTO_REPLY: process.env.AUTO_REPLY || "true",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Awais-star-a11y/TESTING-REPO/raw/refs/heads/main/IMG-20250409-WA0093.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "Yar Zinda Hon",
+    ANTI_LINK: process.env.ANTI_LINK || "false",
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "flase",
+    BOT_NAME: process.env.BOT_NAME || "HASAN ᴍᴅ",
+    ANTI_VV: process.env.ANTI_VV || "true",
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'same' if you want to resend deleted message in same chat
+    OWNER_NAME: process.env.OWNER_NAME || "HASAN", //replace with your name 
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923190217441",// add your bot owner number
+    DEV: process.env.DEV || "923190217441", //replace with your whatsapp number
+    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "true",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`𝒚𝒐𝒖𝒓 𝒔𝒕𝒂𝒕𝒖𝒔 𝒔𝒆𝒆𝒏 𝒋𝒖𝒔𝒕 𝒏𝒐𝒘 𝒃𝒚 𝒂𝒘𝒂𝒊𝒔 𝒎𝒅`",
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+    };
+    
